9ec2a0c91cfc4e8a96e40615bd1a127215d30dd5
MUYUNDO BRIAN <muyundobrian@gmail.com>
2025-06-16 08:11:05 +0300

    updated url and timeout

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,14 +1,14 @@
-Cypress.Commands.add( 'baseurl' , () => {
-    cy.visit("https://nextgen.ngi-test.iner.gy/");
-    cy.wait(9000)
-    cy.contains("with").click();
-    //
-    cy.origin(
-      "https://ngi-auth-ngi-test.auth.eu-west-1.amazoncognito.com",
-      () => {
-      cy.get(".panel-right-border > :nth-child(1) > :nth-child(1) > :nth-child(1) > form > div > .btn").click();
-      }
-    )
+Cypress.Commands.add('baseurl', () => {
+  cy.visit("https://nextgen.ngi-test.iner.gy/")
+  cy.contains('Sign in to continue', {timeout: 10000} )
+    .should('be.visible')
+  cy.contains("with").click()
+   cy.origin("https://ngi-auth-ngi-test.auth.eu-west-1.amazoncognito.com", () => {
+    cy.get(".panel-right-border form .btn").click();
+  })
   
 })
 
+ Cypress.on("uncaught:exception", (err, runnable) => {
+      return false;
+    })
