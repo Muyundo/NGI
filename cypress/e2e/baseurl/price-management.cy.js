9b271d3fe4f391692048ec612eff3a90120793ef
MUYUNDO BRIAN <muyundobrian@gmail.com>
2025-08-06 14:54:51 +0300

    added intercepts to handle api waits for customer bands and groups

--- cypress/e2e/baseurl/price-management.cy.js
+++ cypress/e2e/baseurl/price-management.cy.js
@@ -3,17 +3,29 @@ beforeEach(() => {
 })
 
 describe('visit dashboard', () => {
-  it('Create new price', () => {
+    it('Create new price', () => {
+    // Broader intercept patterns
+    cy.intercept('GET', '**/pricesmanagement/api/v1/bands*').as('getBands')
+    cy.intercept('GET', '**/pricesmanagement/api/v1/groups*').as('getGroups')
+
     cy.contains('Price Management', { timeout: 10000 }).click()
     cy.contains('Prices').should('be.visible').click()
     cy.wait(1000)
-    cy.contains('NEW PRICE', { timeout: 10000 }).should('be.visible').click()
-    cy.get('#generalInfo_type').click()   
+
+    cy.contains('button', 'NEW PRICE')
+      .should('be.visible')
+      .click()
+
+    cy.wait('@getBands', { timeout: 15000 })
+    cy.wait('@getGroups', { timeout: 15000 })
+
+
+    cy.get('#generalInfo_type', {timeout: 10000}).click()   
      cy.get('.ant-select-item-option-content')
       .contains('Depot')
       .click()
     cy.wait(1000)
-    cy.get('#generalInfo_name').type(`Test Price ${Math.floor(Math.random() * 1000)}`)
+    cy.get('#generalInfo_name', {timeout: 10000}).type(`Test Price ${Math.floor(Math.random() * 1000)}`)
     cy.get('.ant-select-selection-overflow').click()
       .type('Training Depot')
     cy.get('.ant-select-item-option-content')
@@ -24,6 +36,21 @@ describe('visit dashboard', () => {
       .contains('Commercial')
       .click()
     cy.contains('NEXT STEP').click()
+
+    cy.get('#productPrice_product').click()
+    cy.get('.ant-select-item-option-content')
+      .contains('LPG')
+      .click()
+    cy.get('#productPrice_pricingOption')
+    cy.get('.ant-select-item-option-content')
+      .contains('Discount')
+      .click()
+    cy.get('#productPrice_amount').type('50')
+    cy.get('#productPrice_startDate').click()
+    cy.get('.ant-picker-cell-in-view')
+      .contains('Today')
+      .click()
+
   
   })
 })
